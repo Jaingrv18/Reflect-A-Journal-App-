da0f23fcec4d0164379f6067b3fc1fdafb22aefe
Garv Jain <garvjain.1304@gmail.com>
2024-12-29 13:10:09 +0530

    Prep for Deployment

---
+++ app/(main)/journal/write/layout.js
@@ -0,0 +1,26 @@
+import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
+import React, { Suspense } from 'react'
+import { BarLoader } from 'react-spinners'
+
+const WriteLayout = ({children}) => {
+  return (
+    <div className='container mx-auto px-4 py-8'>
+        <div className='text-sm text-orange-600 hover:text-orange-700 cursor-pointer' href='/dashboard'>
+        <Link
+          href="/dashboard"
+          className="text-sm text-orange-600 hover:text-orange-700 cursor-pointer"
+        >
+          ← Back to Dashboard
+        </Link>
+        </div>
+
+        <Suspense fallback={<BarLoader color="orange" width={"100%"} /> }>
+        {children}
+        </Suspense>
+
+    </div>
+  )
+}
+
+export default WriteLayout
